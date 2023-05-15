6cabcec8d2f0968068dd0c82b9b403dc3d813b48
Ignacio Caramuto <ignaciocaramuto@gmail.com>
2023-05-15 17:45:00 -0300

    update: implementacion de login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthenticationService } from 'src/app/authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  loginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  constructor(private authService: AuthenticationService) { }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+      this.authService.logIn(this.loginForm.value);
+    } else {
+      // Handle form validation errors
+      console.log('Invalid form');
+    }
+  }
 }
